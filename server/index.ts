e996337fbb3313445bf92bdb4c8f02913be24790
carllllld <carl@pompeius.eu>
2026-01-19 10:19:51 +0000

    4

--- server/index.ts
+++ server/index.ts
@@ -42,7 +42,7 @@ app.use(session({
   saveUninitialized: false,
   name: "connect.sid",
   cookie: { 
-    secure: isProduction,
+    secure: true,
     httpOnly: true,
     sameSite: isProduction ? "none" : "lax",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
