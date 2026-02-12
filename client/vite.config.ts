da17d731ffaa93291aa33bf7b84fc32cb51c9ed0
car219921-creator <car219921@gmail.com>
2026-02-12 22:24:50 +0100

    Fix vite config - disable sourcemap and add esbuild target

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,12 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
+    rollupOptions: {
+      external: [],
+    },
+  },
+  esbuild: {
+    target: 'es2020',
   },
 })
